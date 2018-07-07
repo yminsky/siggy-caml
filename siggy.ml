0fba8713b012f2629d3d4910e0f3213e7178199b
Yaron Minsky <yminsky@janestreet.com>
2018-07-07 16:26:54 -0400

    resolved

--- siggy.ml
+++ siggy.ml
@@ -3,4 +3,4 @@ open Import
 let snigbot x y = (x + y) :: [1;2;3]
 
 let sigatron = snigbot 12321 (-12321)
-let () = print_s [%sexp (sigatron:int list)]
+let () = print_s [%sexp (sigatron:int list)]>>>>>>> destination
